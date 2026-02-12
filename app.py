4bd03291a4e7d0cd0cb927a5810655bfc34fa81a
Lucas Ramos <lcsramos.r@gmail.com>
2026-02-12 06:03:03 -0300

    evoluindo...

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from http import HTTPStatus
+import asyncio
+from datetime import datetime
 
 from models import Message
 
@@ -14,10 +16,23 @@ load_dotenv()
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
+pc: str = os.environ.get("PINECONE_KEY")
 
 app = FastAPI()
 
-@app.get("/", status_code= HTTPStatus.OK, response_model = Message)
-def index():
-    return {'message': 'hello world'}
+user_buffer = {}
+user_id = 1
+
+async def process_buffer(user_id):
+    await asyncio.sleep(40)
+    messages = user_buffer.pop(user_id, [])
+    if messages:
+        print(f'Processando mensagens {messages}')
+
+
+@app.post("/message", status_code= HTTPStatus.OK)
+async def message(user_id: str, message: str):
+    user_buffer.setdefault(user_id, []).append(message)
+    asyncio.create_task(process_buffer(user_id))
+    return {"status": "Mensagem adicionada"}
 
