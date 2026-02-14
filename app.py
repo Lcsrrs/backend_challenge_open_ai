b2c9134763a3781c2d643cb97dbf2c30690732f2
Lucas Ramos <lcsramos.r@gmail.com>
2026-02-14 19:11:34 -0300

    integrando supabase e iniciando pinecone

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from http import HTTPStatus
 import asyncio
+import time
+from json import dumps
 from datetime import datetime
-
-from models import Message
 
 from fastapi import FastAPI
 
 import os
 from supabase import create_client, Client
+from pinecone import Pinecone
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -16,22 +17,35 @@ load_dotenv()
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
-pc: str = os.environ.get("PINECONE_KEY")
+
+pc: str = Pinecone(os.environ.get("PINECONE_KEY"))
+index = pc.Index("backend-challenge")
 
 app = FastAPI()
 
 user_buffer = {}
-user_id = 1
 
 async def process_buffer(user_id):
-    await asyncio.sleep(40)
+    await asyncio.sleep(5)
     messages = user_buffer.pop(user_id, [])
     if messages:
-        print(f'Processando mensagens {messages}')
+        #supabase
+        dt = datetime.now()
+        dt_str = dt.strftime("%Y-%m-%d %H:%M:%S")
+        dt_json = dumps(dt_str)
+        response = supabase.table("Memory").insert({"user_id": user_id, "message": messages, "time_stamp": dt_json}).execute()
+
+        #pinecone
+        upsert_data = [{"_id": f'message{i}', "__default__": text} for i, text in enumerate(messages, start=1)]
+        index.upsert_records(user_id, upsert_data)
+        time.sleep(10)
+        stats = index.describe_index_stats()
+        print(stats)
+        print(f'Processando mensagens {response}')
 
 
 @app.post("/message", status_code= HTTPStatus.OK)
-async def message(user_id: str, message: str):
+async def message(user_id: int, message: str):
     user_buffer.setdefault(user_id, []).append(message)
     asyncio.create_task(process_buffer(user_id))
     return {"status": "Mensagem adicionada"}
